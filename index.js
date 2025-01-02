cbb17c6bec13b9ae826744a548c2070592976fd3
duapuluhsatu21 <nuraliaaa2109@gmail.com>
2025-01-02 17:53:25 +0700

    ed

---
+++ index.js
@@ -0,0 +1,15 @@
+import{renderHTML, setInner, onClick} from 'https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/element.js';
+
+onClick("github",github);
+
+//function github(){
+  //  window.open('https://github.com/duapuluhsatu21');
+//}
+
+//function github(){
+ //  renderHTML ('github','/render.html');
+//}
+
+function youtube(){
+    setInner('github',"harap melihat profile github ku!")
+}
---
+++ main.js
@@ -0,0 +1,15 @@
+import{renderHTML, setInner, onClick} from 'https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/element.js';
+
+onClick("youtube",youtube);
+
+function youtube(){
+    window.open('https://www.youtube.com/@IndonesiaCodeAcademy-g3e');
+}
+
+//function youtube(){
+ //   renderHTML ('youtube','/render.html');
+//}
+
+//function youtube(){
+  //  setInner('youtube',"harap menyaksikan video kami di youtube!")
+//}
