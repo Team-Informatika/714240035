d33f3a52c24640800baaf52a6c400d0fca92aa81
duapuluhsatu21 <nuraliaaa2109@gmail.com>
2025-02-08 11:13:05 +0700

    nah

--- data.js
+++
@@ -1,8 +0,0 @@
-// {
-//     "name": "Nama Anda",
-//     "age": "Usia Anda",
-//     "location": "Lokasi Anda",
-//     "bio": "Deskripsi singkat tentang diri Anda.",
-//     "image": "https://example.com/profile.jpg"
-//   }
-  
